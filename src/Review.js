1cf23280faf1154dd2e7ac802e700b1762ee67e7
saqib nazir <saqib_9090@yahoo.com>
2022-07-10 12:37:55 +0100

    random person button

--- src/Review.js
+++ src/Review.js
@@ -25,6 +25,13 @@ const Review = () => {
       return checkNumber(newIndex);
     });
   };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
   return (
     <article className="review">
       <div className="img-container">
@@ -43,8 +50,10 @@ const Review = () => {
         <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
-        <button className="random-btn">surprise me</button>
       </div>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
     </article>
   );
 };
